d46b6a615ab9dc4ae92ab8a6d2074180d0c9869c
yuyaamano23 <nba.chako.nba@icloud.com>
2022-01-09 18:44:00 +0900

    フォームを1にし、ラジオボタンでカテゴリーを分別

--- work/public/index.php
+++ work/public/index.php
@@ -20,169 +20,161 @@ $event_todos = $todo->getAll("event");
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="utf-8">
   <title>My Todos</title>
   <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
-  <div class = "six_Todos">
-  <div>
-    <!-- 勉強 -->
-    <main data-token="<?= Utils::h($_SESSION['token']); ?>">
-      <header>
-        <h1>勉強</h1>
-        <span class="purge">Purge</span>
-      </header>
+  <div class="six_Todos">
+    <div>
+      <!-- 勉強 -->
+      <main data-token="<?= Utils::h($_SESSION['token']); ?>">
+        <header>
+          <h1>勉強</h1>
+          <span class="purge">Purge</span>
+        </header>
+        <ul>
+          <?php foreach ($study_todos as $todo) : ?>
+            <li data-id="<?= Utils::h($todo->id); ?>">
+              <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+              <span><?= Utils::h($todo->title); ?></span>
+              <span class="delete">x</span>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </main>
 
-      <form id="form-study">
-        <input type="text" name="title" placeholder="Type new todo.">
-      </form>
+      <!-- 部活動 -->
+      <main data-token="<?= Utils::h($_SESSION['token']); ?>">
+        <header>
+          <h1>部活動</h1>
+          <span class="purge">Purge</span>
+        </header>
+        <ul>
+          <?php foreach ($club_todos as $todo) : ?>
+            <li data-id="<?= Utils::h($todo->id); ?>">
+              <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+              <span><?= Utils::h($todo->title); ?></span>
+              <span class="delete">x</span>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </main>
 
-      <ul>
-        <?php foreach ($study_todos as $todo): ?>
-        <li data-id="<?= Utils::h($todo->id); ?>">
-          <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
-          <span><?= Utils::h($todo->title); ?></span>
-          <span class="delete">x</span>
-        </li>
-        <?php endforeach; ?>
-      </ul>
-    </main>
+      <!-- 友達 -->
+      <main data-token="<?= Utils::h($_SESSION['token']); ?>">
+        <header>
+          <h1>友達</h1>
+          <span class="purge">Purge</span>
+        </header>
+        <ul>
+          <?php foreach ($friend_todos as $todo) : ?>
+            <li data-id="<?= Utils::h($todo->id); ?>">
+              <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+              <span><?= Utils::h($todo->title); ?></span>
+              <span class="delete">x</span>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </main>
+    </div>
 
-    <!-- 部活動 -->
-    <main data-token="<?= Utils::h($_SESSION['token']); ?>">
-      <header>
-        <h1>部活動</h1>
-        <span class="purge">Purge</span>
-      </header>
+    <div>
+      <!-- 恋愛  -->
+      <main data-token="<?= Utils::h($_SESSION['token']); ?>">
+        <header>
+          <h1>恋愛</h1>
+          <span class="purge">Purge</span>
+        </header>
+        <ul>
+          <?php foreach ($love_todos as $todo) : ?>
+            <li data-id="<?= Utils::h($todo->id); ?>">
+              <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+              <span><?= Utils::h($todo->title); ?></span>
+              <span class="delete">x</span>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </main>
 
-      <form id="form-club">
-        <input type="text" name="title" placeholder="Type new todo.">
-      </form>
+      <!-- 進路  -->
+      <main data-token="<?= Utils::h($_SESSION['token']); ?>">
+        <header>
+          <h1>進路</h1>
+          <span class="purge">Purge</span>
+        </header>
+        <ul>
+          <?php foreach ($course_todos as $todo) : ?>
+            <li data-id="<?= Utils::h($todo->id); ?>">
+              <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+              <span><?= Utils::h($todo->title); ?></span>
+              <span class="delete">x</span>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </main>
 
-      <ul>
-        <?php foreach ($club_todos as $todo): ?>
-        <li data-id="<?= Utils::h($todo->id); ?>">
-          <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
-          <span><?= Utils::h($todo->title); ?></span>
-          <span class="delete">x</span>
-        </li>
-        <?php endforeach; ?>
-      </ul>
-    </main>
-
-    <!-- 友達 -->
-    <main data-token="<?= Utils::h($_SESSION['token']); ?>">
-      <header>
-        <h1>友達</h1>
-        <span class="purge">Purge</span>
-      </header>
-
-      <form id="form-friend">
-        <input type="text" name="title" placeholder="Type new todo.">
-      </form>
-
-      <ul>
-        <?php foreach ($friend_todos as $todo): ?>
-        <li data-id="<?= Utils::h($todo->id); ?>">
-          <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
-          <span><?= Utils::h($todo->title); ?></span>
-          <span class="delete">x</span>
-        </li>
-        <?php endforeach; ?>
-      </ul>
-    </main>
+      <!-- 学校行事  -->
+      <main data-token="<?= Utils::h($_SESSION['token']); ?>">
+        <header>
+          <h1>学校行事</h1>
+          <span class="purge">Purge</span>
+        </header>
+        <ul>
+          <?php foreach ($event_todos as $todo) : ?>
+            <li data-id="<?= Utils::h($todo->id); ?>">
+              <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+              <span><?= Utils::h($todo->title); ?></span>
+              <span class="delete">x</span>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </main>
+    </div>
   </div>
-
-  <div>
-  <!-- 恋愛  -->
-    <main data-token="<?= Utils::h($_SESSION['token']); ?>">
-      <header>
-        <h1>恋愛</h1>
-        <span class="purge">Purge</span>
-      </header>
-
-      <form id="form-love">
-        <input type="text" name="title" placeholder="Type new todo.">
-      </form>
-
-      <ul>
-        <?php foreach ($love_todos as $todo): ?>
-        <li data-id="<?= Utils::h($todo->id); ?>">
-          <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
-          <span><?= Utils::h($todo->title); ?></span>
-          <span class="delete">x</span>
-        </li>
-        <?php endforeach; ?>
-      </ul>
-    </main>
-
-    <!-- 進路  -->
-    <main data-token="<?= Utils::h($_SESSION['token']); ?>">
-      <header>
-        <h1>進路</h1>
-        <span class="purge">Purge</span>
-      </header>
-
-      <form id="form-course">
-        <input type="text" name="title" placeholder="Type new todo.">
-      </form>
-
-      <ul>
-        <?php foreach ($course_todos as $todo): ?>
-        <li data-id="<?= Utils::h($todo->id); ?>">
-          <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
-          <span><?= Utils::h($todo->title); ?></span>
-          <span class="delete">x</span>
-        </li>
-        <?php endforeach; ?>
-      </ul>
-    </main>
-
-    <!-- 学校行事  -->
-    <main data-token="<?= Utils::h($_SESSION['token']); ?>">
-      <header>
-        <h1>学校行事</h1>
-        <span class="purge">Purge</span>
-      </header>
-
-      <form id="form-event">
-        <input type="text" name="title" placeholder="Type new todo.">
-      </form>
-
-      <ul>
-        <?php foreach ($event_todos as $todo): ?>
-        <li data-id="<?= Utils::h($todo->id); ?>">
-          <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
-          <span><?= Utils::h($todo->title); ?></span>
-          <span class="delete">x</span>
-        </li>
-        <?php endforeach; ?>
-      </ul>
-    </main>
-  </div>
-
-  </div>
+  <form>
+    <input type="text" name="title" placeholder="Type new todo.">
+    <label>
+      <input type="radio" name="category" value="study" checked>勉強
+    </label>
+    <label>
+      <input type="radio" name="category" value="club">部活
+    </label>
+    <label>
+      <input type="radio" name="category" value="friend">友達
+    </label>
+    <label>
+      <input type="radio" name="category" value="love">恋愛
+    </label>
+    <label>
+      <input type="radio" name="category" value="course">進路
+    </label>
+    <label>
+      <input type="radio" name="category" value="event">学校行事
+    </label>
+  </form>
   <script src="js/main.js">
   </script>
 </body>
 <footer>
-<p id="myid"></p>
-    <script>
-        t = 0;
-        var elem = document.getElementById("myid");
- 
-        function myfunc(){
-            var date = new Date();
-            var hours = date.getHours();
-            var minutes = date.getMinutes();
-            var seconds = date.getSeconds();
-            elem.innerHTML = hours+":"+minutes+":"+seconds+;
-        }
- 
-        t = setInterval("myfunc()", 500);
- 
-    </script>
-  </footer>
+  <p id="myid"></p>
+  <script>
+    t = 0;
+    var elem = document.getElementById("myid");
+
+    function myfunc() {
+      var date = new Date();
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var seconds = date.getSeconds();
+      elem.innerHTML = hours + ":" + minutes + ":" + seconds;
+    }
+
+    t = setInterval("myfunc()", 500);
+  </script>
+</footer>
+
 </html>
--- work/public/js/main.js
+++ work/public/js/main.js
@@ -1,99 +1,104 @@
-'use strict';
+"use strict";
 
 {
-  const token = document.querySelector('main').dataset.token;
-  const input = document.querySelector('[name="title"]');
-  const ul = document.querySelector('ul');
+	const token = document.querySelector("main").dataset.token;
+	const input = document.querySelector('[name="title"]');
+	const ul = document.querySelector("ul");
 
-  input.focus();
+	input.focus();
 
-  ul.addEventListener('click', e => {
-    if (e.target.type === 'checkbox') {
-      fetch('?action=toggle', {
-        method: 'POST',
-        body: new URLSearchParams({
-          id: e.target.parentNode.dataset.id,
-          token: token,
-        }),
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('This todo has been deleted!');
-        }
+	ul.addEventListener("click", (e) => {
+		if (e.target.type === "checkbox") {
+			fetch("?action=toggle", {
+				method: "POST",
+				body: new URLSearchParams({
+					id: e.target.parentNode.dataset.id,
+					token: token,
+				}),
+			})
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error("This todo has been deleted!");
+					}
 
-        return response.json();
-      })
-      .then(json => {
-        if (json.is_done !== e.target.checked) {
-          alert('This Todo has been updated. UI is being updated.');
-          e.target.checked = json.is_done;
-        }
-      })
-      .catch(err => {
-        alert(err.message);
-        location.reload();
-      });
-    }
+					return response.json();
+				})
+				.then((json) => {
+					if (json.is_done !== e.target.checked) {
+						alert("This Todo has been updated. UI is being updated.");
+						e.target.checked = json.is_done;
+					}
+				})
+				.catch((err) => {
+					alert(err.message);
+					location.reload();
+				});
+		}
 
-    if (e.target.classList.contains('delete')) {
-      if (!confirm('Are you sure?')) {
-        return;
-      }
-      
-      fetch('?action=delete', {
-        method: 'POST',
-        body: new URLSearchParams({
-          id: e.target.parentNode.dataset.id,
-          token: token,
-        }),
-      });
+		if (e.target.classList.contains("delete")) {
+			if (!confirm("Are you sure?")) {
+				return;
+			}
 
-      e.target.parentNode.remove();
-    }
-  });
+			fetch("?action=delete", {
+				method: "POST",
+				body: new URLSearchParams({
+					id: e.target.parentNode.dataset.id,
+					token: token,
+				}),
+			});
 
-  // study用todo追加処理
-  document.getElementById("form-study").addEventListener("submit", (e) => {
+			e.target.parentNode.remove();
+		}
+	});
+
+	// study用todo追加処理
+	document.querySelector("form").addEventListener("submit", (e) => {
 		e.preventDefault();
 
 		const title = input.value;
 
+    // チェックされているラジオボタンの値を取得
+    let elements = document.getElementsByName("category");
+		let len = elements.length;
+		let checkValue = "";
+		for (let i = 0; i < len; i++) {
+			if (elements.item(i).checked) {
+				checkValue = elements.item(i).value;
+			}
+		}
 		fetch("?action=add", {
 			method: "POST",
 			body: new URLSearchParams({
 				title: title,
-        category: "study",
+				category: checkValue,
 				token: token,
 			}),
 		})
 			.then((response) => response.json())
-			.then((json) => {
-				addTodo(json.id, title);
-			});
-
 		input.value = "";
 		input.focus();
-    location.reload();
+		location.reload();
 	});
 
-  const purge = document.querySelector('.purge');
-  purge.addEventListener('click', () => {
-    if (!confirm('Are you sure?')) {
-      return;
-    }
-    
-    fetch('?action=purge', {
-      method: 'POST',
-      body: new URLSearchParams({
-        token: token,
-      }),
-    });
+	const purge = document.querySelector(".purge");
+	purge.addEventListener("click", () => {
+		if (!confirm("Are you sure?")) {
+			return;
+		}
 
-    const lis = document.querySelectorAll('li');
-    lis.forEach(li => {
-      if (li.children[0].checked) {
-        li.remove();
-      }
-    });
-  });
+		fetch("?action=purge", {
+			method: "POST",
+			body: new URLSearchParams({
+				token: token,
+			}),
+		});
+
+		const lis = document.querySelectorAll("li");
+		lis.forEach((li) => {
+			if (li.children[0].checked) {
+				li.remove();
+			}
+		});
+	});
 }
