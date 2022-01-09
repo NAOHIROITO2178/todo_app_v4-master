da18b27f108d4d8c63e8d20623c924eb54928715
yuyaamano23 <nba.chako.nba@icloud.com>
2022-01-09 23:36:21 +0900

    deleteクラスがついている要素を全て取得し、単体削除機能追加

--- work/public/js/main.js
+++ work/public/js/main.js
@@ -7,35 +7,42 @@
 
 	input.focus();
 
-	ul.addEventListener("click", (e) => {
-		if (e.target.type === "checkbox") {
-			fetch("?action=toggle", {
-				method: "POST",
-				body: new URLSearchParams({
-					id: e.target.parentNode.dataset.id,
-					token: token,
-				}),
-			})
-				.then((response) => {
-					if (!response.ok) {
-						throw new Error("This todo has been deleted!");
-					}
+	// ul.addEventListener("click", (e) => {
+	// 	if (e.target.type === "checkbox") {
+	// 		fetch("?action=toggle", {
+	// 			method: "POST",
+	// 			body: new URLSearchParams({
+	// 				id: e.target.parentNode.dataset.id,
+	// 				token: token,
+	// 			}),
+	// 		})
+	// 			.then((response) => {
+	// 				if (!response.ok) {
+	// 					throw new Error("This todo has been deleted!");
+	// 				}
 
-					return response.json();
-				})
-				.then((json) => {
-					if (json.is_done !== e.target.checked) {
-						alert("This Todo has been updated. UI is being updated.");
-						e.target.checked = json.is_done;
-					}
-				})
-				.catch((err) => {
-					alert(err.message);
-					location.reload();
-				});
-		}
+	// 				return response.json();
+	// 			})
+	// 			.then((json) => {
+	// 				if (json.is_done !== e.target.checked) {
+	// 					alert("This Todo has been updated. UI is being updated.");
+	// 					e.target.checked = json.is_done;
+	// 				}
+	// 			})
+	// 			.catch((err) => {
+	// 				alert(err.message);
+	// 				location.reload();
+	// 			});
+	// 	}
+	// });
 
-		if (e.target.classList.contains("delete")) {
+
+
+  // イベントリスナを複数要素にまとめて登録する(クラス名から取得)
+  // deleteクラスがついている要素を全て取得
+	const delete_buttons = document.getElementsByClassName("delete");
+	for (let delete_button of delete_buttons) {
+		delete_button.addEventListener("click", (e) => {
 			if (!confirm("Are you sure?")) {
 				return;
 			}
@@ -48,9 +55,10 @@
 				}),
 			});
 
-			e.target.parentNode.remove();
-		}
-	});
+      e.target.parentNode.remove();
+			e.preventDefault(); //イベントのキャンセル
+		});
+	}
 
 	// study用todo追加処理
 	document.querySelector("form").addEventListener("submit", (e) => {
@@ -58,8 +66,8 @@
 
 		const title = input.value;
 
-    // チェックされているラジオボタンの値を取得
-    let elements = document.getElementsByName("category");
+		// チェックされているラジオボタンの値を取得
+		let elements = document.getElementsByName("category");
 		let len = elements.length;
 		let checkValue = "";
 		for (let i = 0; i < len; i++) {
@@ -74,8 +82,7 @@
 				category: checkValue,
 				token: token,
 			}),
-		})
-			.then((response) => response.json())
+		}).then((response) => response.json());
 		input.value = "";
 		input.focus();
 		location.reload();
