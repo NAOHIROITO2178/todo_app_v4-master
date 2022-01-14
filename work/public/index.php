95f26ff9adf538ee63973e063c871cc1df7a3c23
NAOHIROITO2178 <korokoro78@ezweb.ne.jp>
2022-01-14 18:29:16 +0900

    デジタルカレンダー追加

--- work/public/index.php
+++ work/public/index.php
@@ -161,4 +161,23 @@ $event_todos = $todo->getAll("event");
   </script>
 </body>
 <footer>
+<span id="view_time"></span>
+
+<script type="text/javascript">
+document.getElementById("view_time").innerHTML = getNow();
+
+function getNow() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var mon = now.getMonth()+1; //１を足すこと
+	var day = now.getDate();
+	var hour = now.getHours();
+	var min = now.getMinutes();
+
+	//出力用
+	var s = year + " " + mon + "/" + day + " " + hour + ":" + min; 
+	return s;
+}
+</script>
+</footer>
 </html>
